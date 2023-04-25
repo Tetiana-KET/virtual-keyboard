a0470d93601c674402d8ebd34a7fc83a148d1468
Tetiana-KET <belangelphone@gmail.com>
2023-04-25 22:05:04 +0300

    chore: add some rules to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,16 @@ module.exports = {
         jest: true,
     },
     rules: {
-        "no-console": 0,
-        "import/prefer-default-export": 0,
-        "prefer-template": 0,
+        "no-console": warn,
+        "import/prefer-default-export": warn,
+        "prefer-template": warn,//Require template literals instead of string concatenation
+        "no-var": "error",//Require let or const instead of var
+        "semi": "error",//Require or disallow semicolons instead of ASI
+        "indent": ["error", 2],//Enforce consistent indentation
+        "no-multi-spaces": "error",//Disallow multiple spaces
+        "space-in-parens": "error",//Enforce consistent spacing inside parentheses
+        "no-multiple-empty-lines": "error",//Disallow multiple empty lines
+        "prefer-const": "error",//Require const declarations for variables that are never reassigned after declared
+        "no-use-before-define": "error"//Disallow the use of variables before they are defined
     },
 };
