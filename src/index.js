5a83db2a2c437040e46a7570365a532cd5b632e1
Tetiana-KET <belangelphone@gmail.com>
2023-04-27 12:28:08 +0300

    feat(idex.js, style.css): add textarea

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ const wrapper = document.createElement("div");
 wrapper.classList.add("wrapper");
 document.body.prepend(wrapper);
 
+//TITLE
 const title = document.createElement("h1");
 title.classList.add("title");
 title.textContent = "RSS Virtual keyboard";
 wrapper.append(title);
+
+//TEXT AREA
+const textArea = document.createElement("textarea");
+textArea.classList.add("text-area");
+wrapper.append(textArea);
+
+//KEYBORD
