2a4a2a8c9c0a723553c765d067118c2cf0965f96
Tetiana-KET <belangelphone@gmail.com>
2023-04-27 14:44:45 +0300

    feat(idex.js): add keyboard wrapper

--- src/index.js
+++ src/index.js
@@ -14,3 +14,18 @@ textArea.classList.add("text-area");
 wrapper.append(textArea);
 
 //KEYBORD
+const keyBoard = document.createElement("div");
+keyBoard.classList.add("key-board");
+wrapper.append(keyBoard);
+
+//KEYBORD wrapper for keys
+const keyBoardKeys = document.createElement("div");
+keyBoardKeys.classList.add("key-board__keys");
+keyBoard.append(keyBoardKeys);
+
+//KEY
+const key = document.createElement("button");
+key.setAttribute("type", "button");
+key.classList.add("key-board__key");
+key.textContent = "a";
+keyBoardKeys.append(key);
