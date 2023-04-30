8ef6a99d0a507197c5899e5f724ecafdcd129008
Tetiana-KET <belangelphone@gmail.com>
2023-04-30 09:21:14 +0300

    refactor(index.js): wrap code into a function keyboardInit call the function on DOMContentLoaded

--- src/index.js
+++ src/index.js
@@ -1,98 +1,104 @@
-const wrapper = document.createElement("div");
-wrapper.classList.add("wrapper");
-document.body.prepend(wrapper);
+function keyboardInit () {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("wrapper");
+  document.body.prepend(wrapper);
+  
+  //TITLE
+  const title = document.createElement("h1");
+  title.classList.add("title");
+  title.textContent = "RSS Virtual keyboard";
+  wrapper.append(title);
+  
+  //TEXT AREA
+  const textArea = document.createElement("textarea");
+  textArea.classList.add("text-area");
+  wrapper.append(textArea);
+  
+  //KEYBORD
+  const keyBoard = document.createElement("div");
+  keyBoard.classList.add("key-board");
+  wrapper.append(keyBoard);
+  
+  //KEYBORD wrapper for keys
+  const keyBoardKeys = document.createElement("div");
+  keyBoardKeys.classList.add("key-board__keys-container");
+  keyBoard.append(keyBoardKeys);
+  
+  //value for text area
+  let value = "";
+  //caps
+  let caps = false;
+  //KEY
+  const keyList = [
+    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
+    "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "/", "del",
+    "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
+    "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "▲", "Shift",
+    "Ctrl", "win", "Alt", "space", "Alt", "◄", "▼", "►", "Ctrl"
+  ];
 
-//TITLE
-const title = document.createElement("h1");
-title.classList.add("title");
-title.textContent = "RSS Virtual keyboard";
-wrapper.append(title);
+  const letters = [
+    "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
+    "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "/", 
+    "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'",
+    "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "▲",
+    "◄", "▼", "►",
+  ];
 
-//TEXT AREA
-const textArea = document.createElement("textarea");
-textArea.classList.add("text-area");
-wrapper.append(textArea);
+  keyList.forEach((elem, i, arr) => {
+    const specialKeys = ["backspace", "Tab", "del", "CapsLock", "Enter", "Shift", "Ctrl", "win", "Alt", "space"].indexOf(elem) !== -1;
+    const widerKeys = ["CapsLock", "Enter", "Shift"].indexOf(elem) !== -1;
 
-//KEYBORD
-const keyBoard = document.createElement("div");
-keyBoard.classList.add("key-board");
-wrapper.append(keyBoard);
+    const key = document.createElement("button");
+    key.setAttribute("type", "button");
+    key.classList.add("key-board__key");
+    key.textContent = arr[i];
+    keyBoardKeys.append(key);
 
-//KEYBORD wrapper for keys
-const keyBoardKeys = document.createElement("div");
-keyBoardKeys.classList.add("key-board__keys-container");
-keyBoard.append(keyBoardKeys);
-
-//value for text area
-let value = "";
-//caps
-let caps = false;
-
-//KEY
-const keyList = [
-  "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
-  "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "/", "del",
-  "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter",
-  "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "", "Shift",
-  "Ctrl", "win", "Alt", "space", "Alt", "", "", "", "Ctrl"
-];
-
-const letters = [
-  "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=",
-  "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "/", 
-  "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'",
-  "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "",
-  "", "", "",
-];
-
-keyList.forEach((elem, i, arr) => {
-  const specialKeys = ["backspace", "Tab", "del", "CapsLock", "Enter", "Shift", "Ctrl", "win", "Alt", "space"].indexOf(elem) !== -1;
-  const widerKeys = ["CapsLock", "Enter", "Shift"].indexOf(elem) !== -1;
-
-  const key = document.createElement("button");
-  key.setAttribute("type", "button");
-  key.classList.add("key-board__key");
-  key.textContent = arr[i];
-  keyBoardKeys.append(key);
-
-  if (specialKeys || elem === "") {
-    key.classList.add("key-board__key_special");
-  }
-  if (elem === "del" || elem === "Tab") {
-    key.classList.add("key-board__key_wide");
-  }
-  if (widerKeys) {
-    key.classList.add("key-board__key_wider");
-  }
-  if (elem === "space") {
-    key.classList.add("key-board__key_extra-wide");
-    key.addEventListener("click", () => {
-      value += " ";
-    });
-  }
-  if (elem === "backspace") {
-    key.classList.add("backspace");
-    key.addEventListener("click", () => {
-      value = value.substring(0, value.length - 1);
-    });
-  }
-  if(elem === "CapsLock") {
-    key.classList.add("key-board__key_caps");
-    key.addEventListener("click", () => {
-      key.classList.toggle("key-board__key_active-caps");
-      caps = !caps;
-    });
-  }
-  if (elem === "Enter") {
-    key.addEventListener("click", () => {
-      value += "\n";
-    });
-  }
-  key.addEventListener("click", (e) => {
-    if (letters.includes(e.target.textContent)) {
-      value += caps ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+    if (specialKeys || elem === "") {
+      key.classList.add("key-board__key_special");
     }
-    
-    console.log(value);
+    if (elem === "del" || elem === "Tab") {
+      key.classList.add("key-board__key_wide");
+    }
+    if (widerKeys) {
+      key.classList.add("key-board__key_wider");
+    }
+    if (elem === "space") {
+      key.classList.add("key-board__key_extra-wide");
+      key.addEventListener("click", () => {
+        value += " ";
+      });
+    }
+    if (elem === "backspace") {
+      key.classList.add("backspace");
+      key.addEventListener("click", () => {
+        value = value.substring(0, value.length - 1);
+      });
+    }
+    if(elem === "CapsLock") {
+      key.classList.add("key-board__key_caps");
+      key.addEventListener("click", () => {
+        key.classList.toggle("key-board__key_active-caps");
+        caps = !caps;
+      });
+    }
+    if (elem === "Enter") {
+      key.addEventListener("click", () => {
+        value += "\n";
+      });
+    }
+    key.addEventListener("click", (e) => {
+      if (letters.includes(e.target.textContent)) {
+        value += caps ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+      }
+      
+      console.log(value);
+    });
   });
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  keyboardInit();
 });
